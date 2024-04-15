7b126d7f7300117c634fb8e9457129abeeadbc73
lucasgomes06 <lu.antunes040508@gmail.com>
2024-04-15 09:04:57 -0300

    Aula 03

---
+++ Dowhile.java
@@ -0,0 +1,10 @@
+public class Dowhile {
+    public static void main(String[] args){
+        int = 5;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while (i < 5);
+    }
+}
---
+++ aula03/Ternarios.java
@@ -0,0 +1,8 @@
+public class ternarios{
+    public static void main(String[] args){
+        int time = 20;
+        String result;
+        result = (time < 18) ? "Good day" : "Good Evening";
+        System.out.println(result);
+    }
+}
