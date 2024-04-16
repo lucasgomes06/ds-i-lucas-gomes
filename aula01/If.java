9541c1b556fee955e400533da2d4bc5b62c24130
lucasgomes06 <lu.antunes040508@gmail>
2024-04-16 07:39:12 -0300

    Aula DS Com Java

---
+++ aula01/If.java
@@ -0,0 +1,14 @@
+public class If {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 20;
+
+        if (x>y) {
+            System.out.println("x é maior que y");
+        } else if (x == y) {
+            System.out.println("x é igual a y");
+        } else {
+            System.out.println("y é maior que x");
+        }
+    }
+}
---
+++ aula01/Input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Input {
+    public static void main (String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        String msg;
+
+        System.out.println("Insira a mensagem");
+        msg = myObj.nextLine();
+
+        System.out.println(msg);
+    }
+
+}
